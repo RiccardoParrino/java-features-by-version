c56ca562ae7af938d8e98931ca05794b74194f58
RiccardoParrino <riccardoparrino1@gmail.com>
2025-11-15 09:14:14 +0100

    add StringLiteral example

---
+++ java21/StringLiteral.java
@@ -0,0 +1,21 @@
+import static java.lang.StringTemplate.STR;
+
+public class StringLiteral {
+
+    public static void main(String[] args) {
+        SimpleExample.example();
+    }
+    // javac --enable-preview --release 21 StringLiteral.java
+    // java --enable-preview StringLiteral
+
+}
+
+class SimpleExample {
+
+    public static void example() {
+        String name = "Riccardo";
+        String welcomeText = STR."Welcome from \{name}";
+        System.out.println(welcomeText);
+    }
+
+}
